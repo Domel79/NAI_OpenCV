d73f2e602c08aac11bec621335fbeac5f0d48c4c
Domel79 <domel.gk@gmail.com>
2016-01-24 23:07:39 +0100

    as

--- Projekt_NAI/main.cpp
+++ Projekt_NAI/main.cpp
@@ -10,13 +10,13 @@ int main(){
 	VideoCapture cap;
 	cap.open(0);
 
-	namedWindow("window",1);
+	namedWindow("window1",1);
 
 	while(1){
 
 		cap>>image;
 
-		imshow("window", image);
+		imshow("window1", image);
 
 		waitKey(33);
 
