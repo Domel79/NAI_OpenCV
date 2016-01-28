9e08f5b519ad493f7cddd54f03c8204267d53a2e
Domel79 <domel.gk@gmail.com>
2016-01-28 23:47:47 +0100

    Wykrywanie ruchu

--- Projekt_NAI/main.cpp
+++ Projekt_NAI/main.cpp
@@ -7,12 +7,13 @@
 
 using namespace cv;
 using namespace std;
-Mat frame;
 
+Mat frame, imgTemp, tmpFrame, tmpFrame2;
+
+BackgroundSubtractorMOG2 bg(100, 100, false);
 
  int main( int argc, char** argv )
- {
-	
+ {	
 
    VideoCapture cap("rtsp://admin:12345@10.133.4.54//Streaming/Channels/2");
    if (!cap.isOpened())
@@ -20,9 +21,19 @@ Mat frame;
 	   cout << "Cannot open the web cam" << endl;
 	   return -1;
     }
-    
+   
+   cap.read(imgTemp);
+
    while(waitKey(30)!=27){
 	  cap>>frame;
+	  vector < std::vector < cv::Point > >contours;
+
+	  bg.operator()(frame,tmpFrame);
+
+	  bg.getBackgroundImage(tmpFrame2);
+	  findContours(tmpFrame, contours,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+	  drawContours(frame,contours, -1,cv::Scalar(255,0,0),2);
+	  
 	  namedWindow("oryginal", WINDOW_AUTOSIZE );
 		imshow("oryginal",frame);
    }
